571d0bc261e688e981f1fe6879b014e536b7b9b0
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-11-12 02:03:57 +0700

    bot

--- idzeroid/__init__.py
+++ idzeroid/__init__.py
@@ -174,7 +174,7 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("idzeroid", API_ID, API_HASH)
-pbot = Client("Idzeroid", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("Idzeroid")
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
